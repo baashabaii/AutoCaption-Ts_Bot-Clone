e9e21992a1806f32f7eb9f59aed8c88157cad80d
baashabaii <86482549+baashabaii@users.noreply.github.com>
2022-01-26 10:00:42 +0530

    try

---
+++ Plugins/tagremove.py
@@ -0,0 +1,34 @@
+import os, asyncio
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+
+bot = Client(
+    "Remove FwdTag",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+@bot.on_message(filters.channel & filters.forwarded)
+async def fwdrmv(c, m):
+    try:
+        if m.media and not (m.video_note or m.sticker):
+            await m.copy(m.chat.id, caption = m.caption if m.caption else None)
+            await m.delete()
+        else:
+            await m.copy(m.chat.id)
+            await m.delete()
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+
+
+@bot.on_message(filters.private | filters.group)
+async def fwdrm(c, m):
+    try:
+        if m.media and not (m.video_note or m.sticker):
+            await m.copy(m.chat.id, caption = m.caption if m.caption else None)
+        else:
+            await m.copy(m.chat.id)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
